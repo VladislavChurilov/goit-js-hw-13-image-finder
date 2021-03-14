5fa7395db20dcddf9a38800c4a0ace7da0c61f38
Vladislav Churilov <88vvch88@gmail.com>
2021-03-14 02:17:32 +0200

    hw13

--- goit-js-hw-13-image-finder/src/js/apiService.js
+++ goit-js-hw-13-image-finder/src/js/apiService.js
@@ -1,11 +1,42 @@
-// const KEY = 20658315-dd3dbd2a0550adf6d5ae10318;
-// const options = {
-//     headers: { key:'20658315-dd3dbd2a0550adf6d5ae10318',
-//     },
-// };
-//  const url = 
-//  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key={fed}';
 
-//  fetch (url, options).then(r => r.json()).then(console.log);
-fetch ('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=20658315-dd3dbd2a0550adf6d5ae10318'
-).then(r => r.json()).then(console.log);
+const KEY = '20658315-dd3dbd2a0550adf6d5ae10318';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class newsApiService{
+    constructor (){
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    fetchPicture(){   
+        // console.log(this);     
+        
+        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+       return fetch(url)
+        .then(picture => picture.json())
+        .then(articles =>{            
+            this.page ++;
+            // this.incrementPage();            
+            return articles; 
+        });
+        
+    }
+    // async fetchPicture () {
+    //     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+    //     const picture = await fetch(url);
+    //     // const increment = await incrementPage();
+    //     const newPicture = await picture.json();
+    //     return newPicture;
+    // }
+    // incrementPage(){
+    //     this.page +=1;
+    // }
+    resetPage(){
+        this.page = 1;
+    }
+    get query(){
+        return this.searchQuery;
+    }
+    set query(newQuery){
+        return this.searchQuery = newQuery;
+    }
+}
--- goit-js-hw-13-image-finder/src/js/pictureFinder.js
+++ goit-js-hw-13-image-finder/src/js/pictureFinder.js
@@ -1,15 +1,67 @@
 import './apiService';
 import pictureTpl from '../templates/picture.hbs';
 const debounce = require('lodash.debounce');
+
+import { alert } from'@pnotify/core';
+import"@pnotify/core/dist/BrightTheme.css";
+
+import NewsApiService from '../js/apiService';
+const newsApiService = new NewsApiService();
+
 const refs = {
     formRef: document.querySelector('#search-form'),
     listRef: document.querySelector('.gallery'),
     loadMoreBtnRef: document.querySelector('.js-load-more-btn')
 }
 
-refs.formRef.addEventListener('input', debounce(onSearch, 500));
 
-function onSearch (e) {
+refs.formRef.addEventListener('input', debounce(onSearch, 1000));
+refs.loadMoreBtnRef.addEventListener('click', loadMore)
+
+  function onSearch (e) {
     e.preventDefault();
-
+    try {
+     clearArticlesConteiner();    
+    newsApiService.query = e.target.value;
+    if (newsApiService.query === ''){        
+        return 
+    }     
+     newsApiService.resetPage();
+    newsApiService.fetchPicture().then(addArticlesMarcup)  
+    } catch (error) {
+        
+    }
+    
+    
 }
+function loadMore() {
+    // const y = e.pageY +2000;
+    // window.scrollTo({
+    //     top: 1000,
+    //     behavior: 'smooth'
+    //   });
+    if (newsApiService.query === ''){
+        const myAlert = alert({                
+            text:"Please enter a more specific query!",        
+            type: 'info'                
+            });
+            return 
+    }      
+    newsApiService.fetchPicture().then(addArticlesMarcup)
+    // .then(window.scrollTo(0, y))   
+}
+function addArticlesMarcup(newPicture) {    
+   return refs.listRef.insertAdjacentHTML('beforeend', pictureTpl(newPicture));    
+}
+function clearArticlesConteiner() {
+    refs.listRef.innerHTML = '';
+}
+// function emptyQuery() {
+//     if (newsApiService.query === ''){
+//         const myAlert = alert({                
+//             text:"Please enter a more specific query!",        
+//             type: 'info'                
+//             });
+//             return 
+//     }
+// }
