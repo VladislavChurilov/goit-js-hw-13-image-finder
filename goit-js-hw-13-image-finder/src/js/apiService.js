2342e1c1864516bcce06f74b5e0a459debec5c5c
Vladislav Churilov <88vvch88@gmail.com>
2021-03-16 14:00:55 +0200

    hw13

--- goit-js-hw-13-image-finder/src/js/apiService.js
+++ goit-js-hw-13-image-finder/src/js/apiService.js
@@ -6,26 +6,14 @@ export default class newsApiService{
     constructor (){
         this.searchQuery = '';
         this.page = 1;
-    }
-    fetchPicture(){   
-        
+    }    
+    async fetchPicture () {
         const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
-       return fetch(url)
-        .then(picture => picture.json())
-        .then(articles =>{            
-            this.page ++;            
-            // this.incrementPage();            
-            return articles; 
-        });        
+        const picture = await fetch(url);
+        const newPicture = await picture.json();        
+        const increment = await (this.incrementPage(newPicture));       
+        return newPicture;
     }
-    // async fetchPicture () {
-    //     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
-    //     const picture = await fetch(url)
-    //     .then (incrementPage());
-        
-    //     const newPicture = await picture.json();
-    //     return newPicture;
-    // }
     incrementPage(){
         this.page +=1;
     }
@@ -39,3 +27,16 @@ export default class newsApiService{
         return this.searchQuery = newQuery;
     }
 }
+
+// use then!!
+// fetchPicture(){   
+        
+    //     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+    //    return fetch(url)
+    //     .then(picture => picture.json())
+    //     .then(articles =>{            
+    //         this.page ++;            
+    //         // this.incrementPage();            
+    //         return articles; 
+    //     });        
+    // }
--- goit-js-hw-13-image-finder/src/js/pictureFinder.js
+++ goit-js-hw-13-image-finder/src/js/pictureFinder.js
@@ -11,53 +11,59 @@ const newsApiService = new NewsApiService();
 // import * as basicLightbox from 'basiclightbox';
 const basicLightbox = require('basiclightbox');
 
-
 const refs = {
-    formRef: document.querySelector('#search-form'),
+    formRef: document.querySelector('.search-form'),   
     listRef: document.querySelector('.gallery'),
-    loadMoreBtnRef: document.querySelector('.js-load-more-btn'),
-    // imgRef: document.querySelector('img')
+    loadMoreBtnRef: document.querySelector('.js-load-more-btn'),    
 }
-refs.formRef.addEventListener('input', debounce(onSearch, 1000));
+// refs.formRef.addEventListener('input', debounce(onSearch, 1000));
+refs.formRef.addEventListener('submit', onSearch)
 refs.loadMoreBtnRef.addEventListener('click', loadMore);
-// refs.listRef.addEventListener('click', modal);
+
 
  async function onSearch (e) {
     e.preventDefault();
     try {
      clearArticlesConteiner();    
-    newsApiService.query = e.target.value;
+    newsApiService.query = e.target.query.value;
     if (newsApiService.query === ''){        
         return 
     }     
     newsApiService.resetPage();
-    const fetch = await newsApiService.fetchPicture()
-    .then(addArticlesMarcup)     
+    const fetch = await newsApiService.fetchPicture();    
+    const marcup = await addArticlesMarcup(fetch);  
+    return marcup;  
     } catch (error) {        
     }    
 }
 async function loadMore() {    
-    // window.scrollTo({
-    //     top: ???,
-    //     behavior: 'smooth'
-    //   });
-    if (newsApiService.query === ''){
+   try {
+        if (newsApiService.query === ''){
         const myAlert = alert({                
             text:"Please enter a more specific query!",        
             type: 'info'                
-            });
-            return 
-    }  
-    const newsApi = await newsApiService.fetchPicture()
-    .then(addArticlesMarcup);    
-    // newsApiService.fetchPicture().then(addArticlesMarcup)
-    // .then(window.scrollTo(0, y))   
+        });
+         return 
+    }        
+        const newsApi = await newsApiService.fetchPicture();  
+        const moreMarcup = await addArticlesMarcup(newsApi)     
+    return await scroll(moreMarcup); 
+   } 
+   catch (error) {       
+    }         
 }
  function addArticlesMarcup(newPicture) {     
    return refs.listRef.insertAdjacentHTML('beforeend', pictureTpl(newPicture));    
 }
 function clearArticlesConteiner() {
     refs.listRef.innerHTML = '';
+}
+function scroll() {
+    window.scrollTo({
+        top:refs.loadMoreBtnRef.offsetTop,
+        left:0,
+        behavior:'smooth'   
+       })       
 }
 // function modal () {    
 //     const instance = basicLightbox.create(`<img src="????????" width="800" height="600">`)
